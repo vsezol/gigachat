790527a3ff95c0fc27b0c177e661c8dcb4b19261
Konstantin Alaev <alaevdev@gmail.com>
2022-11-21 23:20:12 +0300

    add backed

---
+++ packages/backend/src/main/kotlin/ru/alaev/gigachat/BackendApplication.kt
@@ -0,0 +1,11 @@
+package ru.alaev.gigachat
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BackendApplication>(*args)
+}
---
+++ packages/backend/src/test/kotlin/ru/alaev/gigachat/BackendApplicationTests.kt
@@ -0,0 +1,12 @@
+package ru.alaev.gigachat
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
