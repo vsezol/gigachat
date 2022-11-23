bf7084b03b9e775653d54eac491cb1e6f665288f
vsezol <vsezold@gmail.com>
2022-11-24 00:36:04 +0300

    add app-picture and core-messages

---
+++ apps/core-messages/src/main/kotlin/ru/gigachat/coremessages/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package ru.gigachat.coremessages
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(CoreMessagesApplication::class.java)
+	}
+
+}
